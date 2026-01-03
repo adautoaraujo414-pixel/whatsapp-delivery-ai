b13698c347649a3fca22c2d6b32f07d5afba3566
adautoaraujo414-pixel <adautoaraujo414@gmail.com>
2026-01-03 20:36:54 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("🚀 WhatsApp Delivery AI rodando");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
