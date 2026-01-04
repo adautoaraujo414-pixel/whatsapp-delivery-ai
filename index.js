be13be6e55806b8aa2b6e888ebab675d18c98500
Adauto <adautoaraujo414@gmail.com>
2026-01-03 23:37:28 -0300

    fix: ajustar porta para Railway

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.get("/", (req, res) => {
   res.status(200).send("🚀 WhatsApp Delivery AI ONLINE");
 });
 
-// PORTA (Railway ou Local)
-const PORT = process.env.PORT || 3000;
+// PORTA DO RAILWAY
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor rodando na porta", PORT);
